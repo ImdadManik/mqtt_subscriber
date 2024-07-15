b6144ecde1f53c5355be18842e469c45f96abe9e
ImdadManik <imdad.khas@hotmail.com>
2024-07-15 17:29:12 +0300

    fix bugs regarding updating latest setting of devices from iot portal and mqtt subscriber till 15-07-2024

--- iot-project-finalize/pubsub.py
+++ iot-project-finalize/pubsub.py
@@ -52,6 +52,7 @@ payload_data = None
 all_sensor_status = None
 temp_value = None
 temp_alert_freq = None
+min_temp_alert = None
 ldr_value = None
 ldr_alert_freq = None
 
@@ -80,6 +81,7 @@ pub_Door_Topics='sensor/Door/'+ sr.get_raspberry_pi_serial()
 
 #heartbeat topic
 pub_connect_topics = 'device/connected/' + sr.get_raspberry_pi_serial()
+
 # Global variables to track last retain message and state
 last_retain_message = None
 
@@ -110,6 +112,7 @@ def on_message(client, userdata, msg):
     global payload_data
     global temp_value
     global temp_alert_freq
+    global min_temp_alert
     global ldr_value
     global ldr_alert_freq
     global all_sensor_status
@@ -118,54 +121,103 @@ def on_message(client, userdata, msg):
     global account_id
     global device_id
     global last_retain_message
-    print("in on_message event")
 
-    if msg.topic == sub_setting_Topic and msg.payload is not None:   
+    payload_dict=None
+
+
+    if msg.topic == sub_setting_Topic and msg.payload is not None:
+        last_retain_message = msg.payload  # Update last_retain_message
+        payload_data = msg.payload.decode()
+        payload_list = json.loads(payload_data)
+        # Check if payload_list is a list (which indicates it's an array of objects)
+        if isinstance(payload_list, list):
+            if len(payload_list) > 0:  
+                payload_dict = payload_list[0]  # Take the first object from the array
+            else:  
+                print("Error: Empty JSON array received.")
+                return
+        else:
+            payload_dict = payload_list  # It's a single object
+            
         if msg.payload != last_retain_message:
-            last_retain_message = msg.payload  # Update last_retain_message
-            payload_data = msg.payload.decode() 
-            try:
-                payload_list = json.loads(payload_data)
+            temp_value = bool(payload_dict.get('Temp', temp_value))
+            temp_alert_freq = payload_dict.get('TempAlertFreq', temp_alert_freq)
+            min_temp_alert =payload_dict.get('MinTempAlert', min_temp_alert)
+            ldr_value = bool(payload_dict.get('LDR', ldr_value))
+            ldr_alert_freq = payload_dict.get('LDRAlertFreq', ldr_alert_freq)
+            all_sensor_status = bool(payload_dict.get('Status', all_sensor_status))
+            door_value = bool(payload_dict.get('Door', door_value))
+            pir_value = bool(payload_dict.get('PIR', pir_value))
+            account_id = payload_dict.get('AccountId', account_id)
+            device_id =  payload_dict.get('Id', device_id)
+        else:
+            temp_value = bool(payload_dict.get('Temp', temp_value))
+            temp_alert_freq = payload_dict.get('TempAlertFreq', temp_alert_freq)
+            min_temp_alert =payload_dict.get('MinTempAlert', min_temp_alert)
+            ldr_value = bool(payload_dict.get('LDR', ldr_value))
+            ldr_alert_freq = payload_dict.get('LDRAlertFreq', ldr_alert_freq)
+            all_sensor_status = bool(payload_dict.get('Status', all_sensor_status))
+            door_value = bool(payload_dict.get('Door', door_value))
+            pir_value = bool(payload_dict.get('PIR', pir_value))
+            account_id = payload_dict.get('AccountId', account_id)
+            device_id =  payload_dict.get('Id', device_id) 
+        connectedPublishMsg()
+         
+    # if msg.topic == sub_setting_Topic and msg.payload is not None:   
+    #     if msg.payload != last_retain_message:            
+    #         last_retain_message = msg.payload  # Update last_retain_message
+    #         payload_data = msg.payload.decode()
+    #         try:
+    #             payload_list = json.loads(payload_data)
+    #             # Check if payload_list is a list (which indicates it's an array of objects)
+    #             if isinstance(payload_list, list):
+    #                 if len(payload_list) > 0:  
+    #                     payload_dict = payload_list[0]  # Take the first object from the array
+    #                 else:  
+    #                     print("Error: Empty JSON array received.")
+    #                     return
+    #             else:
+    #                 payload_dict = payload_list  # It's a single object 
+ 
+    #             temp_value = bool(payload_dict.get('Temp', temp_value))
+    #             temp_alert_freq = payload_dict.get('TempAlertFreq', temp_alert_freq)
+    #             min_temp_alert =payload_dict.get('MinTempAlert', min_temp_alert)
+    #             ldr_value = bool(payload_dict.get('LDR', ldr_value))
+    #             ldr_alert_freq = payload_dict.get('LDRAlertFreq', ldr_alert_freq)
+    #             all_sensor_status = bool(payload_dict.get('Status', all_sensor_status))
+    #             door_value = bool(payload_dict.get('Door', door_value))
+    #             pir_value = bool(payload_dict.get('PIR', pir_value))
+    #             account_id = payload_dict.get('AccountId', account_id)
+    #             device_id =  payload_dict.get('Id', device_id)
+    #             #print("after if payload_dict ")
+    #             #print("")
+    #             #print("---------------------------------------------------------------------------------------------------------")
+    #             #print(f"temp value : {temp_value} | TempAlertFreq: {temp_alert_freq} | ldr value : {ldr_value} | LDRAlertFreq : {ldr_alert_freq} | Status: {all_sensor_status} | Door: {door_value} | PIR: {pir_value} | AccountId: {account_id} | DeviceId: {device_id}")
+    #             #print("---------------------------------------------------------------------------------------------------------")
+    #         except json.JSONDecodeError as e:
+    #             print(f"Error decoding JSON: {e}")
+    #         except Exception as e:
+    #             print(f"Error processing message: {e}")
+    #     else:
+    #         payload_data = msg.payload.decode()
+    #         temp_value = bool(payload_data.get('Temp', temp_value))
+    #         temp_alert_freq = payload_data.get('TempAlertFreq', temp_alert_freq)
+    #         min_temp_alert =payload_data.get('MinTempAlert', min_temp_alert)
+    #         ldr_value = bool(payload_data.get('LDR', ldr_value))
+    #         ldr_alert_freq = payload_data.get('LDRAlertFreq', ldr_alert_freq)
+    #         all_sensor_status = bool(payload_data.get('Status', all_sensor_status))
+    #         door_value = bool(payload_data.get('Door', door_value))
+    #         pir_value = bool(payload_data.get('PIR', pir_value))
+    #         account_id = payload_data.get('AccountId', account_id)
+    #         device_id =  payload_data.get('Id', device_id)
 
-                # Check if payload_list is a list (which indicates it's an array of objects)
-                if isinstance(payload_list, list):
-                    if len(payload_list) > 0:  
-                        payload_dict = payload_list[0]  # Take the first object from the array
-                    else:  
-                        print("Error: Empty JSON array received.")
-                        return
-                else:
-                    payload_dict = payload_list  # It's a single object 
-                
-                temp_value = bool(payload_dict.get('Temp', temp_value))
-                temp_alert_freq = payload_dict.get('TempAlertFreq', temp_alert_freq)
-                ldr_value = bool(payload_dict.get('LDR', ldr_value))
-                ldr_alert_freq = payload_dict.get('LDRAlertFreq', ldr_alert_freq)
-                all_sensor_status = bool(payload_dict.get('Status', all_sensor_status))
-                door_value = bool(payload_dict.get('Door', door_value))
-                pir_value = bool(payload_dict.get('PIR', pir_value))
-                account_id = payload_dict.get('AccountId', account_id)
-                device_id =  payload_dict.get('Id', device_id)
-
-                print("after if payload_dict ")
-                print("")
-                print("---------------------------------------------------------------------------------------------------------")
-                print(f"temp value : {temp_value} | TempAlertFreq: {temp_alert_freq} | ldr value : {ldr_value} | LDRAlertFreq : {ldr_alert_freq} | Status: {all_sensor_status} | Door: {door_value} | PIR: {pir_value} | AccountId: {account_id} | DeviceId: {device_id}")
-                print("---------------------------------------------------------------------------------------------------------")
-                
-                connectedPublishMsg()
-
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON: {e}")
-            except Exception as e:
-                print(f"Error processing message: {e}")
-
+       
 if __name__ == "__main__":
     try: 
         connect_to_broker()
         client.subscribe(sub_setting_Topic)
         client.on_message = on_message
-
+        connectedPublishMsg()
         client.loop_start()
 
         GPIO.add_event_detect(door_sensor_pin, GPIO.BOTH, callback=door_sensor_callback)
@@ -177,30 +229,28 @@ if __name__ == "__main__":
         while True:
             if client.is_connected():
                 if motion_detected:
-                    push_PIR_reading  # Publish motion detection event
+                    push_PIR_reading  #Publish motion detection event 
 
                 if payload_data is not None:
-                    if all_sensor_status:
-                        print(f"Temp : {temp_value}")
-                        if temp_value is True and temp_alert_freq:
-                            if time.time() - start_time_temp >= temp_alert_freq:
-                                client.publish(pub_Temp_Topics, '20,44'  , retain=_retain)
-                                print('----temp')
-                                start_time_temp = time.time()
+                    if all_sensor_status: 
+                        #if temp.getTemperature() is not None:
+                        # if temp_value is True and temp_alert_freq and (temp.getTemperature() > min_temp_alert):
+                        #     if time.time() - start_time_temp >= temp_alert_freq:
+                        #         client.publish(pub_Temp_Topics, str(temp.getTemperature()) +',' + str(temp.getHumidity())  , retain=_retain)
+                        #         print('----temp')
+                        #         start_time_temp = time.time()
 
-                            if ldr_value is True and ldr_alert_freq:
-                                if time.time() - start_time_ldr >= ldr_alert_freq:
-                                    client.publish(pub_Ldr_Topics, light.detect_light(), retain=_retain)
-                                    print('----ldr')
-                                    start_time_ldr = time.time()
-
+                        if ldr_value is True and ldr_alert_freq:
+                                    if time.time() - start_time_ldr >= ldr_alert_freq:
+                                        client.publish(pub_Ldr_Topics, light.detect_light(), retain=_retain)
+                                        print('----ldr')
+                                        start_time_ldr = time.time()
             time.sleep(interval)
-
     except KeyboardInterrupt:
         print("Keyboard interrupt detected. Disconnecting...")
         disconnect_from_broker(diconnectPublishMsg())
     except Exception as e:
         print("An error occurred:", e)
         disconnect_from_broker(diconnectPublishMsg())
-    # finally:
-    #     disconnect_from_broker(diconnectPublishMsg())
+    finally:
+        disconnect_from_broker(diconnectPublishMsg())
